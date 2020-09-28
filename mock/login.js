9e9e21c974e7351a988963615bd11f5c5aaf83a1
likuan <kuankuan.li@yimifudao.com>
2020-09-28 14:23:09 +0800

    up

--- mock/login.js
+++ mock/login.js
@@ -19,204 +19,6 @@ module.exports = {
   //   ]
   // },
   'post /biz/open/copartner/login': { data: 'yimi_1_test1_0rGgCN_1531193922471' },
-  'get /biz/open/copartner/getUserInfo': {
-    data: {
-      shopId: 1,
-      roleType: 1,
-      roleRightList: [
-        {
-          roleType: 1,
-          roleName: 'DARWIM_OPEN_ADD_USER'
-        }
-      ],
-      menuList: [
-        {
-          id: 211982,
-          name: 'CRM',
-          icon: 'team',
-          url: '/crm/',
-          sys: 'DRW',
-          menuList: [
-            {
-              id: 211626,
-              name: '我的leads',
-              icon: null,
-              url: '/crm/myLeads',
-              sys: 'DRW',
-              childMenuList: []
-            },
-            {
-              id: 211627,
-              name: 'leads管理',
-              icon: null,
-              url: '/crm/leadsManage',
-              sys: 'DRW',
-              childMenuList: []
-            },
-            {
-              id: 211627,
-              name: '合同明细',
-              icon: null,
-              url: '/crm/hetong',
-              sys: 'DRW',
-              childMenuList: []
-            },
-            {
-              id: 211627,
-              name: '订单明细（一对一）',
-              icon: null,
-              url: '/crm/orderList',
-              sys: 'DRW',
-              childMenuList: []
-            },
-            {
-              id: 211627,
-              name: '订单明细（小组课）',
-              icon: null,
-              url: '/crm/orderListXBK',
-              sys: 'DRW',
-              childMenuList: []
-            },
-            {
-              id: 211628,
-              name: '试听课明细（一对一）',
-              icon: null,
-              url: '/crm/shiTing',
-              sys: 'CRS',
-              childMenuList: []
-            },
-            {
-              id: 211628,
-              name: '试听课明细（小组课）',
-              icon: null,
-              url: '/crm/shiTingXBK',
-              sys: 'CRS',
-              childMenuList: []
-            },
-            {
-              id: 211628,
-              name: '课程明细',
-              icon: null,
-              url: '/crm/orderLessonDetails',
-              sys: 'DRW',
-              childMenuList: []
-            },
-            {
-              id: 211628,
-              name: '销售人员管理',
-              icon: null,
-              url: '/saleManage',
-              sys: 'MIS',
-              childMenuList: []
-            }
-          ]
-        },
-        {
-          id: 211630,
-          name: 'CRS',
-          icon: null,
-          url: '/crs',
-          sys: 'CRS',
-          menuList: [
-            {
-              id: 211630,
-              name: '我的学员',
-              icon: null,
-              url: '/crm/myStudents',
-              sys: 'CRM',
-              childMenuList: []
-            },
-            {
-              id: 211630,
-              name: '学员管理',
-              icon: null,
-              url: '/crm/studentsManage',
-              sys: 'CRM',
-              childMenuList: []
-            },
-            {
-              id: 211627,
-              name: '合同明细',
-              icon: null,
-              url: '/crm/hetong',
-              sys: 'CRS',
-              childMenuList: []
-            },
-            {
-              id: 211627,
-              name: '订单明细（一对一）',
-              icon: null,
-              url: '/crm/orderList',
-              sys: 'CRS',
-              childMenuList: []
-            },
-            {
-              id: 211628,
-              name: '试听课明细（一对一）',
-              icon: null,
-              url: '/crm/shiTing',
-              sys: 'CRS',
-              childMenuList: []
-            },
-            {
-              id: 211628,
-              name: '课程明细',
-              icon: null,
-              url: '/crm/orderLessonDetails',
-              sys: 'CRS',
-              childMenuList: []
-            }
-          ]
-        },
-        {
-          name: '试听列表',
-          show: true,
-          icon: 'file',
-          url: '/crm/auditionList'
-        }
-        // {
-        //   name: '试听管理',
-        //   show: true,
-        //   icon: 'file',
-        //   url: '/auditManage',
-        // }
-      ],
-      userId: '@id',
-      realName: '@cname',
-      userAccount: 'ymfd',
-      userPhone: '18787879923',
-      positionId: 1,
-      positionName: '技术总裁',
-      deptId: 1,
-      deptName: '技术产品中心'
-    }
-  },
-  //菜单
-  'get /mms/report/catalog': {
-    data: [
-      {
-        id: 1,
-        name: '潜在客户',
-        show: true,
-        icon: 'solution',
-        url: '/'
-      },
-      {
-        id: 2,
-        name: '合作商',
-        show: true,
-        icon: 'solution',
-        url: '/partner'
-      },
-      {
-        id: 3,
-        name: '门店管理',
-        show: true,
-        icon: 'solution',
-        url: '/storeManage'
-      }
-    ]
-  },
   'get /biz/open/copartner/signOut': {
     data: null
   },
@@ -233,87 +35,6 @@ module.exports = {
   'GET /biz/auth/menu': {
     data: [
       {
-        id: 211982,
-        name: 'CRM',
-        icon: 'team',
-        url: '/crm/',
-        sys: 'DRW',
-        menuList: [
-          {
-            id: 211626,
-            name: '我的leads',
-            icon: null,
-            url: '/crm/myLeads',
-            sys: 'DRW',
-            childMenuList: []
-          },
-          {
-            id: 211627,
-            name: 'leads管理',
-            icon: null,
-            url: '/crm/leadsManage',
-            sys: 'DRW',
-            childMenuList: []
-          },
-          {
-            id: 211627,
-            name: '合同明细',
-            icon: null,
-            url: '/crm/hetong',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211627,
-            name: '订单明细（一对一）',
-            icon: null,
-            url: '/crm/orderList',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211627,
-            name: '订单明细（小组课）',
-            icon: null,
-            url: '/crm/orderManage',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211628,
-            name: '试听课明细（一对一）',
-            icon: null,
-            url: '/crm/shiTing',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211628,
-            name: '试听课明细（小组课）',
-            icon: null,
-            url: '/crm/shiTingXBK',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211628,
-            name: '课程明细',
-            icon: null,
-            url: '/crm/orderLessonDetails',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211628,
-            name: '销售人员管理',
-            icon: null,
-            url: '/saleManage',
-            sys: 'CRS',
-            childMenuList: []
-          }
-        ]
-      },
-      {
         id: 211630,
         name: 'CRS',
         icon: null,
@@ -321,30 +42,6 @@ module.exports = {
         sys: 'CRS',
         menuList: [
           {
-            id: 211630,
-            name: '我的学员',
-            icon: null,
-            url: '/crm/myStudents',
-            sys: 'DRW',
-            childMenuList: []
-          },
-          {
-            id: 211630,
-            name: '学员管理',
-            icon: null,
-            url: '/crm/studentsManage',
-            sys: 'DRW',
-            childMenuList: []
-          },
-          {
-            id: 211627,
-            name: '合同明细',
-            icon: null,
-            url: '/crm/hetong',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
             id: 211627,
             name: '订单明细（一对一）',
             icon: null,
@@ -353,18 +50,10 @@ module.exports = {
             childMenuList: []
           },
           {
-            id: 211628,
-            name: '试听课明细（一对一）',
+            id: 211627,
+            name: '佣金管理',
             icon: null,
-            url: '/crm/shiTing',
-            sys: 'CRS',
-            childMenuList: []
-          },
-          {
-            id: 211628,
-            name: '课程明细',
-            icon: null,
-            url: '/crm/orderLessonDetails',
+            url: '/crm/commissionManage',
             sys: 'CRS',
             childMenuList: []
           }
@@ -379,28 +68,25 @@ module.exports = {
         menuList: [
           {
             id: '@id',
-            name: '门店运营总表',
+            name: '门店总表',
             icon: null,
-            url:
-              '/report/storeReport?to=http://report-sugar.yimifudao.com/report/r_ada15-2z5l5z2f-2yyer2/1fcdd2469314c3e6aea0c07e04b453a0',
+            url: '/report/storeReport',
             sys: 'CRS',
             childMenuList: []
           },
           {
             id: '@id',
-            name: '销售个人报表',
+            name: '销售KPI报表',
             icon: null,
-            url:
-              '/report/saleReport?to=http://report-sugar.yimifudao.com/report/r_ada15-7yjt0bbo-2bd35k/cba79de21d6aeb537a7b543c5554861a',
+            url: '/report/ccKPIReport',
             sys: 'CRS',
             childMenuList: []
           },
           {
             id: '@id',
-            name: '客服个人报表',
+            name: '客服KPI报表',
             icon: null,
-            url:
-              '/report/serviceReport?to=http://report-sugar.yimifudao.com/report/r_ada15-4foajbaf-m51ry2/6df71ea62038b49690c8c186e97d7950',
+            url: '/report/crKPIReport',
             sys: 'CRS',
             childMenuList: []
           }
--- src/pages/report/index.js
+++ src/pages/report/index.js
@@ -1,19 +1,5 @@
 export default [
   {
-    title: '通时通次报表',
-    component: require('bundle-loader?lazy&name=[name]!./durationReport/TongHuaTongJi'),
-    exact: true,
-    noLayout: true,
-    path: '/crm/durationReportIfr'
-  },
-  {
-    title: '通时通次报表',
-    exact: true,
-    breakData: [{ title: '首页', url: '/' }, { title: '通时通次报表' }],
-    component: require('bundle-loader?lazy&name=[name]!./durationReport/Layout'),
-    path: '/report/durationReport'
-  },
-  {
     title: '门店总表',
     exact: true,
     breakData: [{ title: '首页', url: '/' }, { title: '门店运营总表' }],
@@ -34,18 +20,4 @@ export default [
     component: require('bundle-loader?lazy&name=[name]!./crKPIReport/crKPI'),
     path: '/report/crKPIReport'
   }
-  /*{
-    title: '销售KPI报表',
-    exact: true,
-    breakData: [{ title: '首页', url: '/' }, { title: '销售KPI报表' }],
-    component: require('bundle-loader?lazy&name=[name]!./durationReport/Layout'),
-    path: '/report/durationReport'
-  },*/
-  /*  {
-    title: '客服KPI报表',
-    exact: true,
-    breakData: [{ title: '首页', url: '/' }, { title: '客服KPI报表' }],
-    component: require('bundle-loader?lazy&name=[name]!./durationReport/Layout'),
-    path: '/report/durationReport'
-  }*/
 ]
